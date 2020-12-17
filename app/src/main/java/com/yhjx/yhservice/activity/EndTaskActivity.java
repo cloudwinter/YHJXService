9592502fc482384d9ab4254e0de82f7da2bf83ba
cloud <1032176134@qq.com>
2020-12-17 22:23:54 +0800

    开工
    开工

---
+++ app/src/main/java/com/yhjx/yhservice/activity/EndTaskActivity.java
@@ -0,0 +1,9 @@
+package com.yhjx.yhservice.activity;
+
+import com.yhjx.yhservice.base.BaseActivity;
+
+/**
+ * 完工
+ */
+public class EndTaskActivity extends BaseActivity {
+}
---
+++ app/src/main/java/com/yhjx/yhservice/activity/StartTaskActivity.java
@@ -0,0 +1,18 @@
+package com.yhjx.yhservice.activity;
+
+import android.os.Bundle;
+
+import com.yhjx.yhservice.base.BaseActivity;
+
+import androidx.annotation.Nullable;
+
+/**
+ * 开工
+ */
+public class StartTaskActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
